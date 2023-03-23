278146cc641e74929a1b042897f61f7fe8c90c57
Steven <ksteve998@gmail.com>
2023-03-23 16:06:35 -0500

    commit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const winningNumber = Math.ceil(Math.random() * 100)
+let winningNumber = Math.ceil(Math.random() * 100)
 let playerGuess = null 
 let head = document.getElementById("title")
 let subH = document.getElementById("subtitle")
-
-
+let guessCounter = document.getElementById("guessCounter")
+let currentCount = 5
 let guessButton = document.getElementById("submit")
+let retry = document.getElementById("retry")
 
+retry.addEventListener("click", function(){
+    currentCount = 5;
+    guessCounter.textContent = currentCount;
+    winningNumber = Math.ceil(Math.random() * 100)
+    guessButton.disabled = false;
+    head.textContent = "Guessing Game"
+    subH.textContent = "Guess a number between 1 - 100"
+})
 guessButton.addEventListener("click", function() {
     let playerGuess = document.getElementById("playerInput").value
     if (isNaN(playerGuess)) {
@@ -13,13 +22,35 @@ guessButton.addEventListener("click", function() {
     } else if (playerGuess < 1 || playerGuess > 100) {
         subH.textContent = "Only numbers between 1 - 100"
     } else {
-        if (playerGuess = winningNumber) {
+        if (currentCount === 1) {
+            head.textContent = "You're fucking stupid"
+            subH.textContent = "Try again"
+            currentCount = currentCount - 1;
+            guessCounter.textContent = currentCount
+            guessButton.disabled = true;
+
+        }
+         else if (playerGuess < winningNumber) {
+            head.textContent = "Your guess was too low."
+            currentCount = currentCount - 1;
+            guessCounter.textContent = currentCount
+
+        }
+        else if (playerGuess > winningNumber){
+            head.textContent = "Your guess was too high."
+            currentCount = currentCount - 1;
+            guessCounter.textContent = currentCount
+        }
+        else if (playerGuess = winningNumber) {
             head.textContent = `You win! The number was ${playerGuess}`
             subH.textContent = "YAAAAAAAY"
-            guessButton.prop("disabled", true)
-        } 
+            guessButton.disabled = true;
     }
+}
 })
 
 
+
+// console.log(guessCounter)
 console.log(winningNumber)
+
